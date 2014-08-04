365fb969621ccf5375941a37061740abc35633e7
Iliq Nikushev <iliq.nikushev@hotmail.com>
2014-08-04 19:36:11 +0300

    player helper methods
    .matches => Returns all matches that the player has taken part in
    .statistics => Returns all statistics of all sets

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,21 @@ class Player < ActiveRecord::Base
   belongs_to :team
   belongs_to :position
   belongs_to :country
+
+  has_many :player_set_scores
+
+  def statistics
+    return self.player_set_scores
+  end
+
+  def matches
+    return Match.
+    joins("left outer join match_set_scores on match_set_scores.match_id = matches.id").
+    joins("left outer join player_set_scores on player_set_scores.match_set_score_id = match_set_scores.id").
+    where("player_set_scores.player_id = #{self.id}").uniq
+  end
+
+  def match_statistics(match)
+    return self.statistics.where(match_id:match.id)
+  end
 end
