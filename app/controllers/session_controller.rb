374dcdc15cbb1694206fe23c20eeab9c12c97366
Miroslav P. Marinov <miroslav12122@gmail.com>
2014-11-26 20:06:10 +0200

    ruby is bad guy

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,6 +1,7 @@
 class SessionController < Devise::SessionsController
   skip_before_filter  :verify_authenticity_token
   skip_before_filter :authenticate_user!, :only => :create
+
   def login
     if current_scout != nil
       redirect_to(after_sign_in_path_for(nil))
@@ -16,6 +17,8 @@ class SessionController < Devise::SessionsController
   end
 
   def create #create new session, called from login form
+    render status:200, 
+    :json => {}
     warden.authenticate!(:scope => resource_name)
     render status:200, 
     :json => {}
