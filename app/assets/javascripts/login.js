7cd50f13f84d8c0136b1d74c77662ebf250451d0
Miroslav P. Marinov <miroslav12122@gmail.com>
2014-11-26 19:33:03 +0200

    login again

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -3,7 +3,8 @@ function login () {
 	 var email = $("#email").val();
 	 $.ajax({
 		  type: "POST",
-		  url: "/create",
+		  url: "create",
+      beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
 		  data: {scout:{ email:email, password:password }},
 		  success: function() {
 		  	window.location.pathname = "/login";
