78716a40858bdf77ad4bf68043ee3ddbd4101a28
Miroslav P. Marinov <miroslav12122@gmail.com>
2014-11-26 19:22:10 +0200

    LOGIN

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -3,7 +3,7 @@ function login () {
 	 var email = $("#email").val();
 	 $.ajax({
 		  type: "POST",
-		  url: "create",
+		  url: "/create",
 		  data: {scout:{ email:email, password:password }},
 		  success: function() {
 		  	window.location.pathname = "/login";
